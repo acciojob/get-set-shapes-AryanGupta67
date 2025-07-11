82fc44f65192c46d8a1beb2947f1f9075a06cf18
Aryan Gupta <124511705+AryanGupta67@users.noreply.github.com>
2025-07-11 21:53:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-//complete this code
-class Rectangle {}
+// Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// Square class inheriting from Rectangle
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Set both width and height to side
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
+// ✅ Attach to window so Cypress can access them
 window.Rectangle = Rectangle;
 window.Square = Square;
